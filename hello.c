d9ef2a12d8d36fa146f57a83d4a352294ecf4c87
kangilkueon <kangilkueon@gmail.com>
2022-12-02 05:11:02 +0000

    For kernel 6.0.10

--- hello.c
+++ hello.c
@@ -5,28 +5,89 @@
 #include <linux/moduleparam.h>
 #include <linux/version.h> 
 #include <linux/kallsyms.h>
-
 #include <linux/sched.h> 
 #include <linux/uaccess.h> 
+#include <linux/kprobes.h>
+#include <linux/bpf.h>
+#include <linux/filter.h>
+
+#include "skel_internal.h"
 
 
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Lakshmanan");
+MODULE_AUTHOR("yeop");
 MODULE_DESCRIPTION("A Simple Hello World module");
 
+#define PROG_NAME_SIZE 64
+
+int ebpf_load(u8 *prog_name, u8 *buf, u32 prog_size, u8 *log_buf, u32 log_buf_size);
 
 static int __init hello_init(void)
 {
 
-	printk(KERN_INFO "Hello world!\n");
+    printk(KERN_INFO "Hello world!\n");
+    int efd = -1;
 
-	return 0;    // Non-zero return means that the module couldn't be loaded.
+    char kernel_buf[1024] = {
+        0xb7, 0x01, 0x00, 0x00, 0x0a, 0x00, 0x00, 0x00,
+        0x6b, 0x1a, 0xfc, 0xff, 0x00, 0x00, 0x00, 0x00,
+        0xb7, 0x01, 0x00, 0x00, 0x72, 0x6c, 0x64, 0x21,
+        0x63, 0x1a, 0xf8, 0xff, 0x00, 0x00, 0x00, 0x00,
+        0x18, 0x01, 0x00, 0x00, 0x48, 0x65, 0x6c, 0x6c,
+        0x00, 0x00, 0x00, 0x00, 0x6f, 0x20, 0x57, 0x6f,
+        0x7b, 0x1a, 0xf0, 0xff, 0x00, 0x00, 0x00, 0x00,
+        0xbf, 0xa1, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x07, 0x01, 0x00, 0x00, 0xf0, 0xff, 0xff, 0xff,
+        0xb7, 0x02, 0x00, 0x00, 0x0e, 0x00, 0x00, 0x00,
+        0x85, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00,
+        0xb7, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x95, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    };
+    int program_size = 104;
+
+    struct bpf_load_and_run_opts opts = {};
+	struct bpf_loader_ctx *ctx;
+    struct bpf_insn *insn;
+	int ctx_sz = sizeof(*ctx);
+	int err;
+    char errstr[128];
+    char data[128];
+	char log_buf[128];
+
+    //char *data;
+    //data = kmalloc(128, GFP_KERNEL);
+
+    memset(errstr, 0, 128);
+    memset(data, 0, 128);
+    memset(log_buf, 0, 128);
+
+	ctx = kmalloc(ctx_sz, GFP_KERNEL);
+    memset(ctx, 0, ctx_sz);
+    ctx->sz = ctx_sz;
+    
+    insn = (struct bpf_insn*)kernel_buf;
+	opts.ctx = ctx;
+    opts.data = (void*)data;
+    opts.data_sz = 128;
+    opts.insns =(void*)insn;
+    opts.insns_sz = program_size;
+    //opts.errstr = errstr;
+
+    efd = bpf_load_and_run(&opts);
+
+    //printk(KERN_INFO "error: %s\n", errstr);
+    printk(KERN_INFO "efd: %d\n", efd);
+
+    kfree(ctx);
+
+    return 0;    // Non-zero return means that the module couldn't be loaded.
 }
+
 
 static void __exit hello_cleanup(void)
 {
-	printk(KERN_INFO "Cleaning up module.\n");
+    printk(KERN_INFO "Cleaning up module.\n");
 }
 
 module_init(hello_init);
--- hello.sh
+++ hello.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 make
-sudo insmod hello.ko
-dmesg
+sudo insmod hello.ko #sym=18446744072266711712
 sudo rmmod hello
+dmesg
